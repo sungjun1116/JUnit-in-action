3050c01f4857ee0d5bf5f5fd5bc26e90c838dadf
황성준 <opr99@naver.com>
2022-02-05 17:48:56 +0900

    11.2 테스트 냄새: 불필요한 테스트 코드

--- src/test/java/com/example/junit/util/SearchTest.java
+++ src/test/java/com/example/junit/util/SearchTest.java
@@ -3,6 +3,7 @@ package com.example.junit.util;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
@@ -12,45 +13,39 @@ import java.util.logging.Level;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class SearchTest {
     @Test
-    public void testSearch() {
-        try {
-            String pageContent = "There are certain queer times and occasions "
-                    + "in this strange mixed affair we call life when a man "
-                    + "takes this whole universe for a vast practical joke, "
-                    + "though the wit thereof he but dimly discerns, and more "
-                    + "than suspects that the joke is at nobody's expense but "
-                    + "his own.";
-            byte[] bytes = pageContent.getBytes();
-            ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
-            // search
-            Search search = new Search(stream, "practical joke", "1");
-            Search.LOGGER.setLevel(Level.OFF);
-            search.setSurroundingCharacterCount(10);
-            search.execute();
-            assertFalse(search.errored());
-            List<Match> matches = search.getMatches();
-            assertThat(matches).isNotNull();
-            assertTrue(matches.size() >= 1);
-            Match match = matches.get(0);
-            assertThat(match.searchString).isEqualTo("practical joke");
-            assertThat(match.surroundingContext).isEqualTo("or a vast practical joke, though t");
-            stream.close();
+    public void testSearch() throws IOException {
+        String pageContent = "There are certain queer times and occasions "
+                + "in this strange mixed affair we call life when a man "
+                + "takes this whole universe for a vast practical joke, "
+                + "though the wit thereof he but dimly discerns, and more "
+                + "than suspects that the joke is at nobody's expense but "
+                + "his own.";
+        byte[] bytes = pageContent.getBytes();
+        ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
+        // search
+        Search search = new Search(stream, "practical joke", "1");
+        Search.LOGGER.setLevel(Level.OFF);
+        search.setSurroundingCharacterCount(10);
+        search.execute();
+        assertFalse(search.errored());
+        List<Match> matches = search.getMatches();
+        assertThat(matches).isNotNull();
+        assertTrue(matches.size() >= 1);
+        Match match = matches.get(0);
+        assertThat(match.searchString).isEqualTo("practical joke");
+        assertThat(match.surroundingContext).isEqualTo("or a vast practical joke, though t");
+        stream.close();
 
-            // negative
-            URLConnection connection =
-                    new URL("http://bit.ly/15sYPA7").openConnection();
-            InputStream inputStream = connection.getInputStream();
-            search = new Search(inputStream, "smelt", "http://bit.ly/15sYPA7");
-            search.execute();
-            assertThat(search.getMatches().size()).isZero();
-            stream.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            fail("exception thrown in test" + e.getMessage());
-        }
+        // negative
+        URLConnection connection =
+                new URL("http://bit.ly/15sYPA7").openConnection();
+        InputStream inputStream = connection.getInputStream();
+        search = new Search(inputStream, "smelt", "http://bit.ly/15sYPA7");
+        search.execute();
+        assertThat(search.getMatches().size()).isZero();
+        stream.close();
     }
 }
