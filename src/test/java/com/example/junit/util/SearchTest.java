9545d53355b4242b5bb8aabfc8d02b5d92212371
황성준 <opr99@naver.com>
2022-02-05 18:23:15 +0900

    11.5 테스트 냄새: 부푼 생성

--- src/test/java/com/example/junit/util/SearchTest.java
+++ src/test/java/com/example/junit/util/SearchTest.java
@@ -19,14 +19,12 @@ public class SearchTest {
 
     @Test
     public void testSearch() throws IOException {
-        String pageContent = "There are certain queer times and occasions "
+        InputStream stream = streamOn("There are certain queer times and occasions "
                 + "in this strange mixed affair we call life when a man "
                 + "takes this whole universe for a vast practical joke, "
                 + "though the wit thereof he but dimly discerns, and more "
                 + "than suspects that the joke is at nobody's expense but "
-                + "his own.";
-        byte[] bytes = pageContent.getBytes();
-        ByteArrayInputStream stream = new ByteArrayInputStream(bytes);
+                + "his own.");
         // search
         Search search = new Search(stream, "practical joke", A_TITLE);
         Search.LOGGER.setLevel(Level.OFF);
@@ -41,12 +39,15 @@ public class SearchTest {
         stream.close();
 
         // negative
-        URLConnection connection =
-                new URL("http://bit.ly/15sYPA7").openConnection();
+        URLConnection connection = new URL("http://bit.ly/15sYPA7").openConnection();
         InputStream inputStream = connection.getInputStream();
         search = new Search(inputStream, "smelt", A_TITLE);
         search.execute();
         assertTrue(search.getMatches().isEmpty());
         stream.close();
+    }
+
+    private InputStream streamOn(String pageContent) {
+        return new ByteArrayInputStream(pageContent.getBytes());
     }
 }
